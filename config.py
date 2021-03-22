a1329010e4855a9af0a2c87024afb00fff956c1c
BogdanovBA <70698943+BogdanovBA@users.noreply.github.com>
2021-03-22 23:04:48 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import os
+import os 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY =os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}".format(
         user=os.environ.get("DB_USER"),
         pw=os.environ.get("DB_PASSWORD"),
@@ -11,5 +11,4 @@ class Config:
         port=os.environ.get("DB_PORT"),
         db=os.environ.get("DB_NAME")
     )
-    SQLALCHEMY_TRACK_MODIFICATIOINS = False
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ main.py
@@ -0,0 +1,11 @@
+from app import app, db
+from app.models import Post, User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db' : db,
+        'app': app,
+        'Post':Post,
+        'User':User,
+    }
